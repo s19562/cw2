ed402b361294a853e10386763ecded88ae83d3fe
s19562 <s19562@pjwstk.edu.pl>
2020-03-09 11:17:31 +0100

    Create cw2

---
+++ Cwiczenie2/Cwiczenie2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cwiczenie2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //ArrayList 
+            var l = new List<string>();
+            //Set 
+            var s = new HashSet<string>();
+            //Map
+            var m = new Dictionary<string, int>();
+
+            //Wczytywaie pliku 
+            string file = "dane.csv";
+
+            FileInfo f = new FileInfo(file);
+
+            using (StreamReader stream = new StreamReader(f.OpenRead()))
+            {
+
+                string line = "";
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] studentWiersz = line.Split(',');
+                    Console.WriteLine(line);
+
+                }
+
+            } // stream.Dispose(); //.... IDisposible
+
+            //XLM 
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                                       new XmlRootAttribute("uczelnia"));
+
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie="Jan",
+                Nazwisko="Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(st);
+            // ... 
+            serializer.Serialize(writer, list);
+
+        }
+    }
+}
---
+++ Cwiczenie2/Cwiczenie2/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cwiczenie2
+{
+    public class Student
+    {
+
+        //prop + tabx2 
+        //ObservableCollestion<T>
+        //MVVM
+
+
+        [XmlElement(elementName:"fname")]
+        public string Imie { get; set; }
+
+        public string Nazwisko { get; set; }
+
+        [XmlAttribute(attributeName: "email")]
+        public string Email { get; set; }
+
+
+    }
+}
